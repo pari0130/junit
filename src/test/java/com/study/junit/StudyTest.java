fb8619c06376b46654e96afdd1cc89d467891bdc
조동휘 <pari0130@jinotech.com>
2021-04-16 09:57:20 +0900

    [1부] junit 테스트이름 표시

--- src/test/java/com/study/junit/StudyTest.java
+++ src/test/java/com/study/junit/StudyTest.java
@@ -4,24 +4,45 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// 테스트 이름 표시 (언더바를 바꿔줌)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    void create(){
+    @DisplayName("스터디 만들기 학습")
+    void create_new_study(){
         Study study = new Study();
         assertNotNull(study);
 
         System.out.println("create");
     }
 
-//    @BeforeAll
-//    void create1(){
-//        System.out.println("create1");
-//    }
-//
-//    @AfterAll
-//
-//    @BeforeEach
-//
-//    @AfterEach
+    @Test
+    @Disabled
+    void create1(){
+        Study study = new Study();
+        assertNotNull(study);
+
+        System.out.println("");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("가장먼저");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("@Test 전");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("@Test 후");
+    }
 }
