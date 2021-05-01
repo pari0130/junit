fed9c0cc6504274618608f1ab6da1d30c10ab7af
donghwi <pari0130@gmail.com>
2021-05-01 19:17:40 +0900

    환경변수 및 OS 등 버전에 따른 테스트 방법

---
+++ src/test/java/com/study/junit/Junit5_00_tmp/Junit5_01_Assertion/StudyTest.java
@@ -0,0 +1,16 @@
+package com.study.junit.Junit5_00_tmp.Junit5_01_Assertion;
+
+import com.study.junit.Study;
+import com.study.junit.StudyStatus;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StudyTest {
+
+    @Test
+    @DisplayName("예외처리 비교")
+    void create_new_study1(){
+    }
+}
--- src/test/java/com/study/junit/Junit5_02_조건에따라테스트실행하기/StudyTest.java
+++ src/test/java/com/study/junit/Junit5_02_조건에따라테스트실행하기/StudyTest.java
@@ -3,15 +3,108 @@ package com.study.junit.Junit5_02_조건에따라테스트실행하기;
 import com.study.junit.Study;
 import com.study.junit.StudyStatus;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 테스트")
+    @DisplayName("WINDOWS, LINUX 테스트 케이스")
+    @EnabledOnOs({OS.WINDOWS, OS.LINUX})
     void create_new_study1(){
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("WINDOWS, LINUX 테스트 케이스 ");
 
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+            Study study = new Study(100);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("PROD".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("PROD".equalsIgnoreCase(test_env));
+            Study study = new Study(10);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+    }
+
+    @Test
+    @DisplayName("MAC 테스트 케이스")
+    @EnabledOnOs(OS.MAC)
+    void create_new_study2(){
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("MAC 테스트 케이스");
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+            Study study = new Study(100);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("PROD".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("PROD".equalsIgnoreCase(test_env));
+            Study study = new Study(10);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+    }
+
+    @Test
+    @DisplayName("JRE 버전 테스트 케이스")
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9})
+    void create_new_study3(){
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("JRE 버전 테스트 케이스");
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+            Study study = new Study(100);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("PROD".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("PROD".equalsIgnoreCase(test_env));
+            Study study = new Study(10);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+    }
+
+    @Test
+    @DisplayName("EnabledIfEnvironmentVariable 테스트 케이스")
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+    void create_new_study4(){
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("EnabledIfEnvironmentVariable 테스트 케이스");
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+            Study study = new Study(100);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("PROD".equalsIgnoreCase(test_env), () -> {
+            System.out.println(test_env + " TEST CASE");
+
+            Assumptions.assumeTrue("PROD".equalsIgnoreCase(test_env));
+            Study study = new Study(10);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
     }
 }
